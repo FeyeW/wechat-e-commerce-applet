837e6d2add953aa2b24189316b52c39e714d388b
13430851417 <412755014@qq.com>
2022-02-16 09:31:00 +0800

    update：购物车更新

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,62 @@
-// pages/goods_detail/index.js
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        goodsObj: {}
     },
-
+    GoodsInfo:{},
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        const { goods_id } = options
+        this.getGoodsDetail(goods_id)
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+   //获取商品详情数据
+    async getGoodsDetail(goods_id) {
+        const goodsObj = await request({ url: "/goods/detail", data: { goods_id } })
+        this.GoodsInfo=goodsObj
+        this.setData({
+            goodsObj
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
+    //点击轮播图，放大预览
+    handlePrevewImage(e)
+    {
+      const urls=this.GoodsInfo.pics.map(v=>v.pics_mid);
+      const current=e.currentTarget.dataset.url;
+      wx.previewImage({
+        urls,
+        current
+      })
     },
+    handleCartAdd()
+    {
+     //获取缓存中的购物车 数组
+     let cart=wx.getStorageSync("cart")||[];
+     //判断是商品对象是否存在于购物车数组中
+     let index=cart.findIndex(v=>v.goods_id=this.GoodsInfo.goods_id);
+     if(index===-1)
+     {
+         //不存在第一次添加
+         this.GoodsInfo.num=1;
+         cart.push(this.GoodsInfo)
+     }
+     else{
+         //已经存在购物车数据 执行num++
+         cart[index].num++
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+     }
+     //把购物车重新添加到缓存中
+     wx.setStorageSync("cart",cart);
+     wx.showToast({
+       title: '加入成功',
+       icon:'success',
+       mask:true
+     })
     }
 })
--- request/index.js
+++ request/index.js
@@ -1,4 +1,12 @@
+let ajaxTimes = 0
 export const request = (params) => {
+    ajaxTimes++
+    //显示加载中
+    wx.showLoading({
+        title: "加载中",
+        mask: true,
+    });
+
     //共同url
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
     return new Promise((resolve, reject) => {
@@ -10,6 +18,12 @@ export const request = (params) => {
             },
             fail: (err) => {
                 reject(err)
+            },
+            complete: () => {
+                ajaxTimes--;
+                if (ajaxTimes === 0) {
+                    wx.hideLoading();
+                }
             }
         });
     })
